56b5c7365e3a9e479dcf3c79530c73492b998ec6
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2022-12-22 15:26:01 +0900

    :sparkles: :: 소개 페이지 헤더 레이아웃 추가

---
+++ src/components/Introduce/style.ts
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const IntroduceContainer = styled.div`
+    width: 100%;
+    height: 900px;
+`;
+
+export const IntroduceHeader = styled.div`
+    width: 100%;
+    height: 200px;
+`;
+
+export const HeaderIn = styled.div`
+    width: 100%;
+    height: 200px;
+    /* background-color: blueviolet; */
+    margin-top: 80px;
+`;
+
+export const IntroTItle = styled.div`
+    width: 800px;
+    height: 150px;
+    /* background-color: aqua; */
+    margin: 0 auto;
+
+    font-family: 'Fredoka One';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 40px;
+    line-height: 48px;
+    text-align: center;
+
+    strong{
+        font-weight: 900;
+    }
+
+    span{
+        background-color: #FFF4CE;
+    }
+`;
+
+export const IntroImg = styled.div`
+    width: 100%;
+    height: 600px;
+    background-color: burlywood;
+`;
